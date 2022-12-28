64b12c6acc7a0c817af39aac4cce8b0b3e2e6b9a
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2022-12-27 18:25:28 -0800

    Looping-range final version

--- looping-range.js
+++ looping-range.js
@@ -1,6 +1,10 @@
 function range(start, end, step) {
-  rangeArray = [];
-  for (let x = start; x <= end; x = x + step) {
+  const rangeArray = [];
+  if (start > end || step < 0 || start === undefined || end === undefined || step === undefined) {
+    return rangeArray;
+  }
+  
+  else for (let x = start; x <= end; x = x + step) {
     rangeArray.push(x);
   }
   return rangeArray;
@@ -9,3 +13,4 @@ function range(start, end, step) {
 console.log(range(0, 10, 2));
 console.log(range(10, 30, 5));
 console.log(range(-5, 2, 3));
+console.log(range(10, 0, 4));
